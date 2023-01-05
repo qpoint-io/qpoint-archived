f354493756c268bfefdb665122fecc628807641f
Tyler Flint <tylerflint@gmail.com>
2023-01-05 07:49:23 -0500

    Implement a simple logging pattern

--- src/context.ts
+++ src/context.ts
@@ -24,6 +24,28 @@ export class Context<Env = any> {
   startedAt   : number            // the epoch timestamp for when this context was started
   htmlRewriter: HTMLRewriter      // a rewriter instance for modifying html as a stream
 
+  // Logging at the edge has the potential to overwhelm collection sinks
+  // and consume bandwidth for any external logging solutions that will
+  // need to submit over http(s). Additionally, setting a context-wide
+  // log facility level will likely produce undesirable results as all
+  // adapters would potentially become very noisy when the intended
+  // result is to simply debug a single adapter.
+  // 
+  // To this end there is only a single log function that all adapters
+  // should (ideally) use, without facility levels. Each adapter should
+  // adopt a pattern whereby the log-level (or perhaps debug-mode) can
+  // be set or enabled through the adapter configuration, context state,
+  // or both.
+  // 
+  // Example: adapter "foo" only logs errors by default. To toggle debug mode,
+  // add a simple adapter that just initializes the state, above "foo" in the stack.
+  // 
+  // export function (ctx, next) {
+  //    ctx.state['foo.debug'] = true 
+  //    return next()
+  // }
+  log: (message: string) => void
+
   constructor(request: Request, env: Env, ctx: ExecutionContext, state: Object = {}) {
     // set the request from the event
     this.request = request;
@@ -57,6 +79,9 @@ export class Context<Env = any> {
 
     // set a timestamp for when we started
     this.startedAt = Date.now()
+
+    // set the default log function to console.log
+    this.log = (message: string) => { console.log(message) }
   }
 
   // A few shortcut getters/setters
